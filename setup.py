cb98b37210962203ae96223bca67c793323441f0
Matt Haggard <haggardii@gmail.com>
2015-03-04 16:59:49 -0700

    Make it work on pip6

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup
-from pip.req import parse_requirements
 
 
 def getVersion():
@@ -8,22 +7,6 @@ def getVersion():
     version_py = os.path.abspath(os.path.join(__file__,
                                  '../crudset/version.py'))
     return r_version.search(open(version_py, 'r').read()).groups()[0]
-
-
-def parseRequirements():
-    reqs = parse_requirements('requirements.txt')
-    packages = []
-    links = []
-    for req in reqs:
-        if req.url:
-            links.append(str(req.url))
-            packages.append(str(req.req))
-        else:
-            packages.append(str(req.req))
-    return packages, links
-
-install_requires, dependency_links = parseRequirements()
-
 
 
 setup(
@@ -35,6 +18,8 @@ setup(
     packages=[
         'crudset', 'crudset.test'
     ],
-    install_requires=install_requires,
-    dependency_links=dependency_links,
+    install_requires=[
+        'alchimia',
+        'SQLAlchemy==0.8.3',
+    ]
 )
